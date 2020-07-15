30e6b21a64ef7fb4437ea4d5fcac8fe0c359e8d9
Alex <alexkey0516@gmail.com>
2020-07-15 18:05:23 -0500

    Primer comit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+const nombre = 'Alejandro!!!';
+saludar(nombre);
---
+++ src/js/componentes.js
@@ -0,0 +1,9 @@
+import '../css/componentes.css';
+
+export const saludar = (nombre) => {
+  console.log('Creando etiqueta h1 en el HTML');
+  const h1 = document.createElement('h1');
+  h1.innerText = `Hola, ${nombre}, ¿Cómo estás?`;
+
+  document.body.append(h1);
+};
